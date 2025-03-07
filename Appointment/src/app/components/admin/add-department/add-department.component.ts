bc5cf7626c6c72293acdd338b884c7fcd604358e
unknown <mrunalgk@gmail.com>
2025-03-07 16:10:01 +0530

    Frontend added

---
+++ Appointment/src/app/components/admin/add-department/add-department.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-department',
+  imports: [],
+  templateUrl: './add-department.component.html',
+  styleUrl: './add-department.component.css'
+})
+export class AddDepartmentComponent {
+
+}
