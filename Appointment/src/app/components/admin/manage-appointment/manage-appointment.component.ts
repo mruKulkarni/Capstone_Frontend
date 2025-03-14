33612111fbb09699437da9c6a0b0aaa070417cb2
Dronanaik <dronanaik1133@gmail.com>
2025-03-14 19:42:06 +0530

    Recent Changes commited

---
+++ Appointment/src/app/components/admin/manage-appointment/manage-appointment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-appointment',
+  imports: [],
+  templateUrl: './manage-appointment.component.html',
+  styleUrl: './manage-appointment.component.css'
+})
+export class ManageAppointmentComponent {
+
+}
